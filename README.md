61d84d5afd3c21ae7c3654315765010f75a5ffa0
Lawrence Snead <Lawrence.Snead@gmail.com>
2020-02-01 15:33:01 -0500

    Basic file structure complete

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Note-Taker
+## User Story
+
+AS A user, I want to be able to write and save notes
+
+I WANT to be able to delete notes I've written before
+
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+
+## Business Context
+
+For users that need to keep track of a lot of information, it's easy to forget or be unable to recall something important. Being able to take persistent notes allows users to have written information available when needed.
---
+++ routes/apiRoutes.js
@@ -0,0 +1,52 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var notesDB = require("../db/db.json");
+var waitListData = require("../data/waitinglistData");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/notes", function(req, res) {
+      res.json(notesDB);
+  });
+
+  app.get("/api/notes:id", function(req, res) {
+    res.json(waitListData);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/notes", function(req, res) {
+
+      notesDB.push(req.body);
+      res.json(notesDB);
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    tableData.length = 0;
+    waitListData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
